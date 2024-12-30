ad0eccd75f7aeb1f645a656d4af5efce076b15c9
Crowdbotics <team@crowdbotics.com>
2024-12-30 05:31:00 +0000

    add package 02exercicio

--- backend/demoapp3012_50519/urls.py
+++ backend/demoapp3012_50519/urls.py
@@ -34,9 +34,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Demoapp3012"
-admin.site.site_title = "Demoapp3012 Admin Portal"
-admin.site.index_title = "Demoapp3012 Admin"
+admin.site.site_header = "Demoapp30122"
+admin.site.site_title = "Demoapp30122 Admin Portal"
+admin.site.index_title = "Demoapp30122 Admin"
 
 # swagger
 urlpatterns += [
